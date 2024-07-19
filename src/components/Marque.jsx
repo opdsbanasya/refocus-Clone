1a41e05dd869b55ae1500312899e9201e9c82ae9
DHARM <opdsbanasya@gmail.com>
2024-07-19 23:22:14 +0530

    img marque added

---
+++ src/components/Marque.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Marque = ({imgUrl}) => {
+    return (
+        <div className='w-full py-10 flex items-center justify-between gap-20 whitespace-nowrap overflow-hidden'>
+            {imgUrl.map((url, index)=>(<img src={url} className='w-[7vw] flex-shrink-0'/>))}
+            {imgUrl.map((url, index)=>(<img src={url} className=''/>))}
+        </div>
+    );
+}
+
+export default Marque;
