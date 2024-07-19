abead942a160fa936baeb8186ad5cace352873e8
DHARM <opdsbanasya@gmail.com>
2024-07-19 22:02:34 +0530

    work component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Work from './components/Work';
 
 const App = () => {
     return (
         <div className='w-full h-screen font-[satoshi_variables]'>
             <Navbar />
+            <Work />
         </div>
     );
 }
