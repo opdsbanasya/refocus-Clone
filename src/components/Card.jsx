fe31dad4a1dc628ad0ebaeaa09055b72f2f4dc43
DHARM <opdsbanasya@gmail.com>
2024-07-27 18:40:57 +0530

    hover effect added on Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { motion } from "framer-motion";
 
-const Card = ({width, start, para, hover = "none"}) => {
+const Card = ({ width, start, para, hover = false }) => {
   return (
-    <div className={`bg-zinc-700 p-5 min-h-[80vh] rounded-xl flex flex-col hover:${hover} hover:p-7 transition-all justify-between ${width}`}>
+    <motion.div
+      whileHover={{ backgroundColor: hover === true && "#8B5CF6", padding:"25px" }}
+      className={`bg-zinc-700 p-5 min-h-[80vh] rounded-xl flex flex-col justify-between ${width}`}
+    >
       <div className="w-full">
         <div className="w-full flex justify-between items-center">
           <h3 className="text-lg">Hadding 1</h3>
@@ -14,11 +18,29 @@ const Card = ({width, start, para, hover = "none"}) => {
         </div>
       </div>
       <div className="down w-full mt-56 space-y-5">
-        <h1 className={`text-6xl font-semibold tracking-tight leading-none ${start === false && "hidden"}`}>Start a project</h1>
-        <button className={`rounded-full py-2 px-4 border-[1px] border-zinc-200 ${start === false && "hidden"}`}>Contact us</button>
-        <p className={`text-sm text-zinc-500 font-medium ${para === false && "hidden"}`}>Explore what drives our team.</p>
+        <h1
+          className={`text-6xl font-semibold tracking-tight leading-none ${
+            start === false && "hidden"
+          }`}
+        >
+          Start a project
+        </h1>
+        <button
+          className={`rounded-full py-2 px-4 border-[1px] border-zinc-200 ${
+            start === false && "hidden"
+          }`}
+        >
+          Contact us
+        </button>
+        <p
+          className={`text-sm text-zinc-500 font-medium ${
+            para === false && "hidden"
+          }`}
+        >
+          Explore what drives our team.
+        </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import Card from './Card';
-
+import React from "react";
+import Card from "./Card";
 
 const Cards = () => {
-    return (
-        <div className='w-full'>
-            <div className="max-w-screen-xl mx-auto py-20 flex gap-2">
-                <Card width={"basis-1/3"} start={false} para={true} />
-                <Card width={"basis-2/3"} start={true} para={false} hover={"bg-[#8B5CF6]"} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="w-full">
+      <div className="max-w-screen-xl mx-auto py-20 flex gap-2">
+        <Card width={"basis-1/3"} start={false} para={true} />
+        <Card
+          width={"basis-2/3"}
+          start={true}
+          para={false}
+          hover={true}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Cards;
